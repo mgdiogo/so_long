80e9ad37460b1bfb0b31fb3fad18d28293a76806
Miguel Diogo <miguel.g.diogo@gmail.com>
2023-04-10 22:33:14 +0100

    The walls are working

--- aux/keyhooks.c
+++ aux/keyhooks.c
@@ -6,24 +6,53 @@
 /*   By: sark <sark@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 16:37:25 by sark              #+#    #+#             */
-/*   Updated: 2023/04/05 22:17:12 by sark             ###   ########.fr       */
+/*   Updated: 2023/04/10 22:32:23 by sark             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
+void    walk_left(void)
+{
+    
+    if (mlx_map()->p_x - PLAYER_SIZE < PLAYER_SIZE)
+        return ;
+    mlx_map()->p_x -= PLAYER_SIZE;
+}
+void    walk_right(void)
+{
+    if (mlx_map()->p_x + PLAYER_SIZE >= mlx_win()->width - PLAYER_SIZE)
+        return ;
+    mlx_map()->p_x += PLAYER_SIZE;
+}
+
+void    walk_up(void)
+{
+    if (mlx_map()->p_y - PLAYER_SIZE < PLAYER_SIZE)
+        return ;
+    mlx_map()->p_y -= PLAYER_SIZE;
+}
+
+void    walk_down(void)
+{
+    if (mlx_map()->p_y + PLAYER_SIZE >= mlx_win()->height - PLAYER_SIZE)
+        return ;
+    mlx_map()->p_y += PLAYER_SIZE;
+}
+
 int eventos(int keycode)
 {
-    if (keycode == 97 || keycode == 65361 || keycode == 100 || keycode == 65363 || keycode == 119 || keycode == 65362 || keycode == 115 || keycode == 65364)
+    if (keycode == 97 || keycode == 65361 || keycode == 100 
+    || keycode == 65363 || keycode == 119 || keycode == 65362 || keycode == 115 || keycode == 65364)
     {
-        if (keycode == 97 || keycode == 65361 && (mlx_map()->p_x - 1 > PLAYER_SIZE))
-            mlx_map()->p_x -= PLAYER_SIZE;
-        if (keycode == 100 || keycode == 65363 && (mlx_map()->map[mlx_map()->p_y][mlx_map()->p_x + 1] != '1'))
-            mlx_map()->p_x += PLAYER_SIZE;
-        if (keycode == 119 || keycode == 65362 && (mlx_map()->map[mlx_map()->p_y - 1][mlx_map()->p_x] != '1'))
-            mlx_map()->p_y -= PLAYER_SIZE;
-        if (keycode == 115 || keycode == 65364 && (mlx_map()->map[mlx_map()->p_y + 1][mlx_map()->p_x] != '1'))
-            mlx_map()->p_y += PLAYER_SIZE;
+        if (keycode == 97 || keycode == 65361)
+            walk_left();
+        if (keycode == 100 || keycode == 65363)
+            walk_right();
+        if (keycode == 119 || keycode == 65362)
+            walk_up();
+        if (keycode == 115 || keycode == 65364)
+            walk_down();
         build_map();
         mlx_put_image_to_window(mlx_win()->mlx, mlx_win()->mlx_win, mlx_map()->player, mlx_map()->p_x, mlx_map()->p_y);
     }
